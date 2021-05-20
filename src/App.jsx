a4350e887514388d920058132e099fdfbd0d1433
Kamila <kamila.cerdeira.kc@gmail.com>
2021-05-20 15:19:08 -0300

    otimizacao de carrregamento com o react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './styles/global.scss'
-import Routes from './Routes'
+import React, { Suspense } from 'react'
+const Routes = React.lazy(() => import('./Routes'))
 
 export function App() {
-    return <Routes />
+    return (
+        <Suspense fallback={<div></div>}>
+            <Routes />
+        </Suspense>
+    )
 }
